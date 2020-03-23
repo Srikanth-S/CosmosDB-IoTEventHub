43c39d1fc3d69147b4f5d7e22b8682e5c1e8d61a
Srikanth-S <31615842+Srikanth-S@users.noreply.github.com>
2020-03-23 21:00:46 +0800

    New File commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function (context, IoTHubMessages) {
+    context.log(`JavaScript eventhub trigger function called for message array: ${IoTHubMessages}`);
+
+    IoTHubMessages.forEach(message => {
+        context.log(`Processed message: ${message}`);
+        context.bindings.outputDocument = message;
+    }); 
+
+    context.done();
+};
